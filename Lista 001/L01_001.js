d0b69a44749374a815f76d11091849cd55c5e0ac
Yusha Mariak Miranda Silva <yushamariak@gmail.com>
2018-01-30 13:48:32 -0200

    Exercise 1 from first list of algorithms exercises

---
+++ Lista 001/L01_001.js
@@ -0,0 +1,20 @@
+var nome      = prompt("Nome: ");
+var idade     = prompt("Idade: ");
+var sexo      = prompt("Sexo: ");
+var altura    = prompt("Altura: ");
+var profissao = prompt("Profissao: ");
+var rua       = prompt("Rua: ");
+var bairro    = prompt("Bairro: ");
+var cidade    = prompt("Cidade: ");
+var estado    = prompt("Estado");
+var cep       = prompt("CEP: ");
+
+
+console.log("==========================================");
+console.log(nome + ", Voce tem " + idade + "anos");
+console.log("com altura de " + altura + " mts");
+console.log("Sua profissao é " + profissao);
+console.log("Mora na rua " + rua);
+console.log("Bairro " + bairro);
+console.log("CEP: " + cep + " - " + cidade + " (" + estado + ")");
+console.log("==========================================");
